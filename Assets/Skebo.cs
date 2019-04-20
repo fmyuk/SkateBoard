944ce5a08f206aac98f299f4ffc5de3fd8f14db7
fumi uk <dcfumiya.238@gmail.com>
2019-04-20 21:54:34 +0900

    スケートボード

---
+++ Assets/Skebo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skebo : MonoBehaviour {
+
+    float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.speed = 0.1f;
+        }
+
+        transform.Translate(this.speed, 0, 0);
+
+        this.speed *= 0.98f;
+    }
+}
